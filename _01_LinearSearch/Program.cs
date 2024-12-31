5a91a1c95c8f8fa68e532ce313ddf00f41ef67f3
Vali Mandeal <valimandeal@yoloftw.local>
2024-12-31 14:27:25 +0200

    Added first 2 projects

---
+++ _01_LinearSearch/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _02SearchingAlgorithms;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ _01_RecursionExample/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _01Recursion;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World Recursion!");
+    }
+}
