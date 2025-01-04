03caf19b483008bdd7a66903cfb7c7ef0b73adca
Vali Mandeal <valimandeal@yoloftw.local>
2025-01-04 14:05:53 +0200

    Added factorial algorithm, and search

--- _01_LinearSearch/Program.cs
+++ _01_LinearSearch/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] array = [22, 5, 3, 2, 8];
+
+        int searchResult = search(array, 8);
+        
+        Console.WriteLine(searchResult);
+    }
+
+    static int search(int[] array, int value)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == value)
+                return i;
+        }
+        
+        return -1;
     }
 }
