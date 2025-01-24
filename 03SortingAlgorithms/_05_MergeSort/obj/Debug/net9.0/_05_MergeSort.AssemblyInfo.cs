0becb2ae5a95ae5b44245db655527e96fe2f81c8
valimandeal <valeriu.mandeal@protonmail.com>
2025-01-24 16:58:26 +0200

    Playing with the project structure

--- 03SortingAlgorithms/_05_MergeSort/obj/Debug/net9.0/_05_MergeSort.AssemblyInfo.cs
+++ 03SortingAlgorithms/_05_MergeSort/obj/Debug/net9.0/_05_MergeSort.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("_05_MergeSort")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ad87b5bd1075c0244727038d6f528c39380b42e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26d33b94e7a3e62c64990697ad08f149d8ec4251")]
 [assembly: System.Reflection.AssemblyProductAttribute("_05_MergeSort")]
 [assembly: System.Reflection.AssemblyTitleAttribute("_05_MergeSort")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
